f33dbeac25641a7e1c929151990cf5fed4a808ed
alectramell <alectramell@gmail.com>
2014-12-12 19:55:54 -0700

    KARMON

---
+++ activate/__main__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os
+
+os.system('clear')
+
+def actions(): 
+    jobs = raw_input('What would you have me do?: ')
+
+    if jobs == ('facebook'):
+       os.system('sensible-browser --new-window="http://www.facebook.com"')
+
+    else:
+       os.sysytem('clear')
+       print "I'm sorry, what?"
+       os.system('sleep 4 && python ~/klib/activate')
