f6e59d804c42a90c10663226dfe935edf99d68d5
alectramell <alectramell@gmail.com>
2014-12-12 20:30:22 -0700

    KARMON

--- activate/__main__.py
+++ activate/__main__.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python
 
 import os
+import getpass
 
 os.system('clear')
 
 def actions(): 
     jobs = raw_input('What would you have me do?: ')
 
+    username = getpass.getuser()
+    username = str(username)
+
     if jobs == ('facebook'):
        os.system('sensible-browser --new-window="http://www.facebook.com"')
+
+    if jobs == ('who are you?'):
+       os.system('clear')
+       print ('I am Karmon, you know that..')
+       os.system('sleep 4 && python ~/klib/activate')
+
+    if jobs == ('who am i?'):
+       os.system('clear')
+       print ('You are ' + username + ', and you operate my library..')
+       os.system('sleep 4 && clear')
+
+    if jobs == ('whoami?'):
+       os.system('clear')
+       print ('You are ' + username + ', and you operate my library..')
+       os.system('sleep 4 && clear')
+
+    if jobs == ('whoami'):
+       os.system('clear')
+       print ('You are ' + username + ', and you operate my library..')
+       os.system('sleep 4 && clear')
 
     if jobs == ('idk'):
        os.system('clear')
