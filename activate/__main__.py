d014b284e498104e14f9a4b865260ac8b0f488d5
alectramell <alectramell@gmail.com>
2014-12-13 05:23:18 -0700

    KARMON

--- activate/__main__.py
+++ activate/__main__.py
@@ -6,14 +6,17 @@ import getpass
 os.system('clear')
 
 def actions(): 
-    jobs = raw_input('Run command(s): ')
+    jobs = raw_input('[GHOST]$')
 
     username = getpass.getuser()
     username = str(username)
 
     os.system('clear')
 
-    os.system('gnome-terminal --title="Karmon v1.0 | Terminal.." -x sh -c "' + jobs + '"')
+    os.system('touch ~/Desktop/karmon.log')
+    log = str('echo ' + jobs + ' >> ~/Desktop/karmon.log')
+
+    os.system(log)
 
 actions()
 
